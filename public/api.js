906d7f3a014726208a51f33a344710d68b845891
Bohdan Pechenyak <bohdan.pechenyak@gmail.com>
2021-01-12 15:19:23 -0500

    fixing bugs

--- public/api.js
+++ public/api.js
@@ -12,6 +12,7 @@ const API = {
 
   async addExercise(data) {
     const id = location.search.split("=")[1];
+    data.id = id;
     const res = await fetch("/api/workouts/" + id, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
